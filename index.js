9c2cc5f33d375154618026a60216ee6bd2513ca4
szabadulomezotur <tislerpeter@tpitechnical.hu>
2025-04-04 18:26:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import BookingSystem from "./BookingSystem";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BookingSystem from './BookingSystem';
 
 ReactDOM.render(
   <React.StrictMode>
     <BookingSystem />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
